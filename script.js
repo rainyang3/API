3fa8c2a6370be0a8d575fea37861b5243a177015
rainyang3 <77300717+rainyang3@users.noreply.github.com>
2021-02-03 06:08:41 +0800

    final

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
+
 let url = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=RpI4gkUAvvPiuL84PIosepa0GWRDW9jm"
 let url2 = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=RpI4gkUAvvPiuL84PIosepa0GWRDW9jm"
 let articlebutton = document.getElementById("articlebutton");
 let moviebutton = document.getElementById("moviebutton");
 let article = document.getElementById("article");
 let movie= document.getElementById("movie");
+
+
 
 $.getJSON (url,function(data){
         console.log (data.results[0].abstract);
